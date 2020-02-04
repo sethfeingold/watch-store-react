1e22b1f26c51898ebc60ccdc5791780f08a58535
SethFeingold <sfeingold1104@gmail.com>
2020-02-04 15:03:27 -0600

    navbar started, styled-components added thru npm install

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+import logo from '../logo.svg';
+import styled from 'styled-components';
 
 export default class Navbar extends Component {
     render() {
         return (
             <div>
-                <h3>Hello from Navbar</h3>
+                <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+                    <Link to="/">
+                        {/* Icons made by Kiranshastry from www.flaticon.com */}
+                        <img src={logo} alt="store"  />
+                    </Link>
+                    <ul className="navbar-nav align-items-center">
+                        <li className="nav-item ml-5">
+                            <Link to="/" className="nav-link">
+                                products
+                            </Link>
+                        </li>
+                    </ul>
+                    <Link to="/cart" className="ml-auto">
+                        <button>
+                            <i className="fas fa-cart-plus"/>
+                            My Cart
+                        </button>
+                    </Link>
+                </nav>
             </div>
         )
     }
