5326b6479931a4318fb4076061245c8046bfef86
SethFeingold <sfeingold1104@gmail.com>
2020-02-04 18:05:24 -0600

    images are being passed down in Product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,7 +9,12 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
                 <div className="card">
-                    
+                    <div className="img-container p-5" onClick={() => console.log('you clicked me on the image container')}>
+                        <Link to="/details">
+                            <img src={img} alt="product" className="card-img-top" />
+                        </Link>
+                        <button className="cart-btn"></button>
+                    </div>
                 </div>
             </ProductWrapper>
         )
